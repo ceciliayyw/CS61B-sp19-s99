d6eb7b7bb1de20619c6c3d61d53baeca39c0b2bd
CeciliaW <cecilia.yyw@gmail.com>
2021-01-08 11:50:15 -0800

    Passed calDistance Class

--- proj0/Body.java
+++ proj0/Body.java
@@ -1,3 +1,4 @@
+import java.lang.Math;
 public class Body {
     double xxPos;
     double yyPos;
@@ -6,16 +7,23 @@ public class Body {
     double mass;
     String imgFileName;
 
-    public Body(double xP, double yP, double xV, double yV, double m, String img){
+    public Body (double xP, double yP, double xV, double yV, double m, String img){
         xxPos = xP;
         yyPos = yP;
         xxVel = xV;
         yyVel = yV;
         mass = m;
         imgFileName = img;
-    };
+    }
 
-    public Body(Body b){
+    public Body(Body b)
+    {
         Body c = new Body(b);
+    }
+
+    public double calcDistance (Body a){
+       double squareofRadius = (a.xxPos - xxPos)*(a.xxPos - xxPos) + (a.yyPos - yyPos)*(a.yyPos - yyPos);
+       double radius = Math.sqrt(squareofRadius);
+       return radius;
     }
 }
