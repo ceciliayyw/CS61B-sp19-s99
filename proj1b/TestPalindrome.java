d941e3099d761c129a37369438da937a0e51bb29
CeciliaW <cecilia.yyw@gmail.com>
2021-02-23 20:47:41 -0800

    resubmit

--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -17,38 +17,28 @@ public class TestPalindrome {
     }
 
     @Test
-    public void testPalindrome() {
-        /** test palindrome */
-        String input1 = "racecar";
-        boolean actualResult1 = palindrome.isPalindrome(input1);
-        assertTrue(actualResult1);
+    public void testIsPalindrome() {
+        assertTrue(palindrome.isPalindrome(""));
+        assertTrue(palindrome.isPalindrome("a"));
+        assertTrue(palindrome.isPalindrome("AAAaAAA"));
+        assertTrue(palindrome.isPalindrome("ABBA"));
+        assertFalse(palindrome.isPalindrome("Aa"));
+        assertFalse(palindrome.isPalindrome("ACDC"));
+        assertFalse(palindrome.isPalindrome("yang"));
+    }
 
-        String input2 = "noon";
-        boolean actualResult2 = palindrome.isPalindrome(input2);
-        assertTrue(actualResult2);
-
-        String input3 = "";
-        boolean actualResult3 = palindrome.isPalindrome(input2);
-        assertTrue(actualResult3);
-
-        String input4 = "a";
-        boolean actualResult4 = palindrome.isPalindrome(input2);
-        assertTrue(actualResult4);
-
-        String input5 = "willow";
-        boolean actualResult5 = palindrome.isPalindrome(input5);
-        assertFalse(actualResult5);
-
-        /** Test OffByOne */
-        assertFalse(palindrome.isPalindrome("racecar", new OffByOne()));
-        assertFalse(palindrome.isPalindrome("noon", new OffByOne()));
-        assertTrue(palindrome.isPalindrome("", new OffByOne()));
-        assertTrue(palindrome.isPalindrome("a", new OffByOne()));
-        assertFalse(palindrome.isPalindrome("willow", new OffByOne()));
-        assertTrue(palindrome.isPalindrome("Detrude", new OffByOne()));
-
-
-        /** Test OffByN */
-        assertTrue(palindrome.isPalindrome("binding", new OffByN(5)));
+    @Test
+    public void testIsPalindromeCC() {
+        OffByOne offByOne = new OffByOne();
+        assertTrue(palindrome.isPalindrome("", offByOne));
+        assertTrue(palindrome.isPalindrome("a", offByOne));
+        assertTrue(palindrome.isPalindrome("flake", offByOne));
+        assertTrue(palindrome.isPalindrome("zyzy", offByOne));
+        assertTrue(palindrome.isPalindrome("yyxz", offByOne));
+        assertTrue(palindrome.isPalindrome("yyyxz", offByOne));
+        assertFalse(palindrome.isPalindrome("aa", offByOne));
+        assertFalse(palindrome.isPalindrome("xyz", offByOne));
+        assertFalse(palindrome.isPalindrome("aa", offByOne));
+        assertFalse(palindrome.isPalindrome("zxzx", offByOne));
     }
 }
